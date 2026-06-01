8e7092a7f420547635a719391e62958403346e65
dalmia01@gmail.com <prateek.dalmia@encora.com>
2026-06-01 22:05:45 +0530

    feat: lighthouse added

---
+++ .lighthouserc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 3,
+      url: [
+        "http://localhost:3000/",
+        "http://localhost:3000/dashboard",
+        "http://localhost:3000/sign-in",
+        "http://localhost:3000/theme",
+        "http://localhost:3000/rendering",
+        "http://localhost:3000/rendering/client",
+        "http://localhost:3000/rendering/static",
+        "http://localhost:3000/rendering/dynamic",
+        "http://localhost:3000/rendering/isr",
+        "http://localhost:3000/rendering/streaming",
+      ],
+      startServerCommand: "npm run build && PORT=3000 npm run start",
+      startServerReadyPattern: "listening|ready|started|Compiled successfully",
+      startServerReadyTimeout: 120000,
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["warn", { minScore: 0.9 }],
+        "categories:accessibility": ["warn", { minScore: 0.9 }],
+      },
+    },
+  },
+};
