030c2e843a26eb8487314de173157bcce2cf1657
dalmia01@gmail.com <prateek.dalmia@encora.com>
2026-06-15 21:13:05 +0530

    feat: merge conflicts resovle

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,4 @@
+export { Badge } from "@/components/atoms/badge/Badge";
+export { Button } from "@/components/atoms/button/Button";
+export { Icon } from "@/components/atoms/icon/Icon";
+export { Input } from "@/components/atoms/input/Input";
---
+++ src/components/templates/ShellLayout/ShellLayout.tsx
@@ -0,0 +1,28 @@
+import type { ReactNode } from "react";
+import { Footer } from "@/components/organisms/footer/Footer";
+import { Navbar } from "@/components/organisms/navbar/Navbar";
+import {
+  FOOTER_CONFIG,
+  HEADER_CONFIG,
+  type LayoutShellId,
+  resolveLayoutShellOptions,
+} from "@/constants/layoutConfig";
+import styles from "./ShellLayout.module.scss";
+
+type ShellLayoutProps = {
+  children: ReactNode;
+  clientKey?: string;
+  layoutId: LayoutShellId;
+};
+
+export const ShellLayout = ({ children, clientKey, layoutId }: ShellLayoutProps) => {
+  const shell = resolveLayoutShellOptions(layoutId, clientKey);
+
+  return (
+    <div className={styles.shell}>
+      {shell.showHeader ? <Navbar config={HEADER_CONFIG} /> : null}
+      <div className={styles.content}>{children}</div>
+      {shell.showFooter ? <Footer config={FOOTER_CONFIG} /> : null}
+    </div>
+  );
+};
