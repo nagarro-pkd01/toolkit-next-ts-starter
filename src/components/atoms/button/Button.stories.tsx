b387c340a047df2527b2b10007b36129bac2fed4
dalmia01@gmail.com <prateek.dalmia@encora.com>
2026-06-15 20:12:48 +0530

    feat: mr conflicts resolved

--- src/components/atoms/button/Button.stories.tsx
+++ src/components/atoms/button/Button.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
-import { Button } from "@/components/atoms/button/Button";
+import { Button } from "@/components/atoms/Button/Button";
 
 const meta: Meta<typeof Button> = {
   title: "Atoms/Button",
